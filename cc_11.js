47b12126a2ba5ccc1818fb14ab473686cb6705df
Aritree Hoque <aritree.hoque@gmail.com>
2025-03-02 20:28:36 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -21,3 +21,27 @@ console.log(book1.getDetails());
 book1.updateCopies(-1);
 console.log(book1.getDetails());
 // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+// Task2- Creating a Borrower Class
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name;
+        this.borrowerId = borrowerId; 
+        this.borrowedBooks = [];
+    };
+    borrowBook(book) {
+        this.borrowedBooks.push(book)
+    };
+    returnBook(book) {
+        this.borrowedBooks = this. borrowedBooks.filter(b => b !== book)
+    };
+};
+// Test Cases
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+// Expected output: ["The Great Gatsby"]
+
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+// Expected output: []
